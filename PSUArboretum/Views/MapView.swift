93ace82f36b57646d946dcf3efd818c0a9d185b5
calvin <calvin@calvins-MacBook-Pro-2.local>
2023-12-26 16:50:39 -0500

    more map camera adjustments. changes the look of the map markers

--- PSUArboretum/Views/MapView.swift
+++ PSUArboretum/Views/MapView.swift
@@ -14,25 +14,31 @@ struct MapView: View {
     
     @State private var region = MapCameraBounds(
         centerCoordinateBounds: MKCoordinateRegion(
-            center: CLLocationCoordinate2D(latitude: 40.8123343, longitude: -77.8771519),
-            span: MKCoordinateSpan(latitudeDelta: 0.5, longitudeDelta: 0.5)
+            center: CLLocationCoordinate2D(latitude: 40.805597, longitude: -77.869007),
+            // Deltas are in lat/lng degrees.... one lat degree is ~111,000 meters soooooo yeah
+            span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
         ),
-        minimumDistance: 5,
-        maximumDistance: 30
+        minimumDistance: 1,
+        maximumDistance: 1500
     )
     
     var body: some View {
         WithViewStore(self.store, observe: { $0 }) { viewStore in
             ZStack(alignment: .bottom) {
                 if viewStore.loading {
-                   ProgressView()
+                    ProgressView()
                 } else {
                     Map(bounds: region, interactionModes: .all, selection: viewStore.$selectedFeatureId) {
                         ForEach(viewStore.features, id: \.id) { feature in
-                            Marker(feature.commonName,
-                                   coordinate: CLLocationCoordinate2D(latitude: feature.latitude,
-                                                                      longitude: feature.longitude))
-                            .tag(feature.id)
+                            Annotation(feature.commonName,
+                                       coordinate: CLLocationCoordinate2D(latitude: feature.latitude,
+                                                                          longitude: feature.longitude),
+                                       anchor: .bottom) {
+                               Circle()
+                                    .foregroundColor(Colors.brightGreen)
+                                    .frame(width: 10, height: 10)
+                            }
+                           .tag(feature.id)
                         }
                     }
                     .mapStyle(.hybrid(elevation: .realistic))
